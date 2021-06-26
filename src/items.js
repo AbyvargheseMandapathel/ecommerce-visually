0f69eb0bbdd68fc0cb19fa4c48e75954decd91e9
Jaskaran Singh <hs123online@gmail.com>
2021-06-26 13:40:17 +0530

    first commit

---
+++ src/items.js
@@ -0,0 +1,83 @@
+import photo1 from '../src/img/product-1.png'
+import photo2 from '../src/img/product-2.png'
+import photo3 from '../src/img/product-3.png'
+import photo4 from '../src/img/product-4.png'
+import photo5 from '../src/img/product-5.png'
+import photo6 from '../src/img/product-6.png'
+import photo7 from '../src/img/product-7.png'
+import photo8 from '../src/img/product-8.png'
+export const storeItems =
+[
+{
+    "id": 1,
+    "name": "Google Pixel Black",
+    "category": "GOOGLE",
+    "priceCents": 1600,
+    "img":photo1,
+    "imageColor": "F00",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  },
+  {
+    "id": 2,
+    "name": "Samsung S7",
+    "category": "SAMSUNG",
+    "img":photo2,
+    "priceCents": 2100,
+    "imageColor": "FF0",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  },
+  {
+    "id": 3,
+    "name": "HTC 10 Black",
+    "category": "HTC",
+    "img":photo3,
+    "priceCents": 1200,
+    "imageColor": "00F",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  },
+  {
+    "id": 4,
+    "name": "HTC 10 White",
+    "category": "HTC",
+    "priceCents": 1800,
+    "img":photo4,
+    "imageColor": "F60",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  },
+  {
+    "id": 5,
+    "name": "HTC Desire 626s",
+    "category": "HTC",
+    "img":photo5,
+    "priceCents": 1600,
+    "imageColor": "0F0",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  },
+  {
+    "id": 6,
+    "name": "Vintage Iphone",
+    "category": "APPLE",
+    "priceCents": 2100,
+    "img":photo6,
+    "imageColor": "60F",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  },
+  {
+    "id": 7,
+    "name": "Iphone 7",
+    "category": "APPLE",
+    "priceCents": 1200,
+    "img":photo7,
+    "imageColor": "AAA",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  },
+  {
+    "id": 8,
+    "name": "Smashed Iphone",
+    "category": "APPLE",
+    "priceCents": 1600,
+    "img":photo8,
+    "imageColor": "333",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure eum facere minima, dolores repudiandae quo animi earum."
+  }
+]
